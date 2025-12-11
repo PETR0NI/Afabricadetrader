16067f77a964f0b91dd0c681d42aafab5fbfddf2
PETR0NI <petr0ni@example.com>
2025-12-11 16:24:30 -0300

    fix: ajusta next.config para compatibilidade com vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  experimental: {
+    outputFileTracingIncludes: {
+      '/': ['./public/**/*'],
+    },
+  },
 }
 
 module.exports = nextConfig
